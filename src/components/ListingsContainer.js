2fb722042775d38477d5018f062a7409c50fc913
Faith Kariuki <tcwamboi@gmail.com>
2024-08-08 16:54:52 +0300

    Complete lab

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,46 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ searchQuery }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((res) => res.json())
+      .then((data) => setListings(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.ok) {
+          setListings(listings.filter((listing) => listing.id !== id));
+        } else {
+          throw new Error("Failed to delete listing");
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const filteredListings = listings.filter((listing) =>
+    listing.description.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {filteredListings.map((listing) => (
+          <li>
+            <ListingCard
+              key={listing.id}
+              listing={listing}
+              onDelete={handleDelete}
+            />
+          </li>
+        ))}
       </ul>
     </main>
   );
